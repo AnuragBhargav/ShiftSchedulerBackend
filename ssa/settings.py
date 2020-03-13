0a87422f671cee6a4a8d991619f68b3a9057025d
Vishnuvardhan Nayakam <vishnuvardhan.nayakam@gspann.com>
2020-03-13 22:42:07 +0530

    commit

--- ssa/settings.py
+++ ssa/settings.py
@@ -140,8 +140,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATIC_URL = '/static/'
+
 # Use nose to run all tests
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 
